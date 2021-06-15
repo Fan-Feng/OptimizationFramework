94f393bb7cda3d08acf77abcdfdfc8c249012316
fan feng <ffeng@tamu.edu>
2021-06-15 14:55:11 -0500

    update

--- Experiment6/runTest.py
+++ Experiment6/runTest.py
@@ -10,3 +10,5 @@ else:
     for procid in range(1,p):
         message = comm.recv(source=procid)
         print("process 0 receives message from process",procid,":",message)
+    with open("Test.txt") as fp:
+        fp.write('success')
