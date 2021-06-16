94b5c3dde77bfaae33d2b291c4da7faf6d305a46
fan feng <ffeng@tamu.edu>
2021-06-16 03:30:07 -0500

    update

--- Experiment7/runTest.py
+++ Experiment7/runTest.py
@@ -1,10 +1,13 @@
-from mpipool import Pool
+from mpipool import MPIPool
 from mpi4py import MPI
 
 def menial_task(x):
   return x ** MPI.COMM_WORLD.Get_rank()
 
-with Pool() as pool:
+with MPIPool() as pool:
+  pool.workers_exit()
+  print("Only the master executes this code.")
+
   # Block for results
   results = pool.map(menial_task, range(100))
 
