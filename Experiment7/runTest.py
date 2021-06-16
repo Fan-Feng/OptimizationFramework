b68621e70eb5aa33c5eff09356bbe4905e686d04
fan feng <ffeng@tamu.edu>
2021-06-16 03:38:15 -0500

    ip

--- Experiment7/runTest.py
+++ Experiment7/runTest.py
@@ -5,16 +5,17 @@ def menial_task(x):
   print(MPI.COMM_WORLD.Get_rank())
   return x ** MPI.COMM_WORLD.Get_rank()
 
-with MPIPool() as pool:
-  pool.workers_exit()
-  print("Only the master executes this code.")
+if __name__ == "__main__":
+    with MPIPool() as pool:
+        pool.workers_exit()
+        print("Only the master executes this code.")
 
-  # Block for results
-  results = pool.map(menial_task, range(10))
-  print(results)
+        # Block for results
+        results = pool.map(menial_task, range(10))
+        print(results)
 
-  # Async
-  result = pool.map_async(menial_task, range(10))
-  print("Done already?", result.ready())
+        # Async
+        result = pool.map_async(menial_task, range(10))
+        print("Done already?", result.ready())
 
-print("All MPI processes join again here.")
+    print("All MPI processes join again here.")
