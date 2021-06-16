c577236c458b46fd3cb2a3f5ff258a9cf13619e5
fan feng <ffeng@tamu.edu>
2021-06-16 02:29:18 -0500

    update

---
+++ Experiment7/runTest.py
@@ -0,0 +1,18 @@
+from mpipool import MPIPool
+from mpi4py import MPI
+
+def menial_task(x):
+  return x ** MPI.COMM_WORLD.Get_rank()
+
+with MPIPool() as pool:
+  pool.workers_exit()
+  print("Only the master executes this code.")
+
+  # Block for results
+  results = pool.map(menial_task, range(100))
+
+  # Async
+  result = pool.map_async(menial_task, range(100))
+  print("Done already?", result.ready())
+
+print("All MPI processes join again here.")
