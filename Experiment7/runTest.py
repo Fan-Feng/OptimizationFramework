af167e567be056369624d1f403af3268bb24295f
fan feng <ffeng@tamu.edu>
2021-06-16 03:48:58 -0500

    ip

--- Experiment7/runTest.py
+++ Experiment7/runTest.py
@@ -2,12 +2,13 @@ from mpipool import MPIPool
 from mpi4py import MPI
 
 def menial_task(x):
-  return x ** MPI.COMM_WORLD.Get_rank()
+    print(MPI.COMM_WORLD.Get_rank())
+    return x ** MPI.COMM_WORLD.Get_rank()
 
 with MPIPool() as pool:
     pool.workers_exit()
     print("Only master")
 
-    results = pool.map(menial_task, range(10))
+    results = pool.map(menial_task, range(4))
 
 print("All MPI processes join again here.")
