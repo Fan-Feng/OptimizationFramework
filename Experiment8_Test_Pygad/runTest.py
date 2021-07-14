f2f928359ab94473d41fa4479ee614433e6d38ac
fan feng <ffeng@tamu.edu>
2021-07-14 15:17:29 -0500

    Update

--- Experiment8_Test_Pygad/runTest.py
+++ Experiment8_Test_Pygad/runTest.py
@@ -1,8 +1,9 @@
 import pygad
 import numpy
 import matplotlib.pyplot as plt
-from multiprocessing import Pool
 
+from mpipool import MPIPool
+from mpi4py import MPI
 """
 Given the following function:
     y = f(w1:w6) = w1x1 + w2x2 + w3x3 + w4x4 + w5x5 + 6wx6
@@ -26,43 +27,52 @@ class PooledGA(pygad.GA):
         #print(pop_fitness)
         pop_fitness = numpy.array(pop_fitness)
         return pop_fitness
-if __name__ == "__main__":
-    numpy.random.seed(1234)
-    with Pool(processes=20) as pool:
-        num_generations = 100 # Number of generations.
-        num_parents_mating = 10 # Number of solutions to be selected as parents in the mating pool.
 
-        sol_per_pop = 39 # Number of solutions in the population.
-        num_genes = 10
+# run optimization 
+with MPIPool() as pool:
+    pool.workers_exit() ## Only master process will proceed
+    
+    num_generations = 100 # Number of generations.
+    num_parents_mating = 10 # Number of solutions to be selected as parents in the mating pool.
 
-        num_parents_mating = 2
-        last_fitness = 0
-        
-        parent_selection_type = "sss"
-        keep_parents = 1
+    sol_per_pop = 39 # Number of solutions in the population.
+    num_genes = 10
 
-        crossover_type = "single_point"
+    num_parents_mating = 2
+    
+    
+    parent_selection_type = "sss"
+    keep_parents = 1
 
-        mutation_type = "random"
-        mutation_num_genes = 1
-        
-        ga_instance = PooledGA(num_generations=num_generations,
-                            initial_population =  numpy.random.randint(-10,10,(20,6)),
-                            num_parents_mating=num_parents_mating,
-                            sol_per_pop=sol_per_pop,
-                            num_genes=num_genes,
-                            fitness_func=fitness_func)
-        # Running the GA to optimize the parameters of the function.
-        ga_instance.run()
+    crossover_type = "single_point"
 
-        #ga_instance.plot_result()
-        plt.plot(ga_instance.best_solutions_fitness)
+    mutation_type = "random"
+    mutation_num_genes = 2
+    
+    ga_instance = PooledGA(num_generations=num_generations,
+                num_parents_mating=num_parents_mating,
+                fitness_func=fitness_func, # Actually this is not used.
+                sol_per_pop=sol_per_pop,
+                num_genes=num_genes,
+                parent_selection_type=parent_selection_type,
+                keep_parents=keep_parents,
+                crossover_type=crossover_type,
+                mutation_type=mutation_type,
+                mutation_num_genes=mutation_num_genes
+                )
+    # Running the GA to optimize the parameters of the function.
+    ga_instance.run()
 
-        # Returning the details of the best solution.
-        solution, solution_fitness, solution_idx = ga_instance.best_solution(ga_instance.last_generation_fitness)
-        print("Parameters of the best solution : {solution}".format(solution=solution))
-        print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
-        print("Index of the best solution : {solution_idx}".format(solution_idx=solution_idx))
+    #ga_instance.plot_result()
+    #plt.plot(ga_instance.best_solutions_fitness)
+    print("Fitness_versus_generation")
+    print(ga_instance.best_solutions_fitness)
+
+    # Returning the details of the best solution.
+    solution, solution_fitness, solution_idx = ga_instance.best_solution(ga_instance.last_generation_fitness)
+    print("Parameters of the best solution : {solution}".format(solution=solution))
+    print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+    print("Index of the best solution : {solution_idx}".format(solution_idx=solution_idx))
 
 
 
