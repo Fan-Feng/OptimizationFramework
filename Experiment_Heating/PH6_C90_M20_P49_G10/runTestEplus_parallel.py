03102c3bdf98bca529d5e782ec36361ee8dc175a
fan feng <ffeng@tamu.edu>
2021-08-18 16:42:41 -0500

    update

--- Experiment_Heating/PH6_C90_M20_P49_G10/runTestEplus_parallel.py
+++ Experiment_Heating/PH6_C90_M20_P49_G10/runTestEplus_parallel.py
@@ -292,8 +292,8 @@ with MPIPool() as pool:
     keep_parents = 1
 
     # Optimization algorithm setting
-    num_generations = 10
-    sol_per_pop = 49   # Number of individuals
+    num_generations = 50
+    sol_per_pop = 99   # Number of individuals
 
     crossover_type = "single_point"
     crossover_probability = 0.9
