1e329aa6bc828e4b18090eb93b9634a4d2bcf38c
fan feng <ffeng@tamu.edu>
2021-09-08 01:05:31 -0500

    update

--- Experiment_Heating/PH1_Oneday/PH1_C90_M20_P100_G50_Feb02/result_Test.py
+++ Experiment_Heating/PH1_Oneday/PH1_C90_M20_P100_G50_Feb02/result_Test.py
@@ -30,7 +30,7 @@ def convert_NumOfSec_To_MonAndDay(NumOfSec):
   Mon = DateValue.month
   DayValue = DateValue.day
 
-  return Mon,DayValue
+  return Mon,DayValue,HourOfDay
 
 def modifyIDF(fileName,targetFile,startMon,startDay,endMon, endDay,SchFileLOC):
   '''
@@ -48,10 +48,10 @@ def modifyIDF(fileName,targetFile,startMon,startDay,endMon, endDay,SchFileLOC):
       line = line.replace("%EndDay%",str(endDay))
       line = line.replace("%SchFile_Loc%",SchFileLOC)
       fp.writelines(line)
-    
+  
 def fitness_func(x,solution_idx):
   # run simulation, this is deprecated. 
-  res = run_prediction(tim,x,CVar_timestep,X_sp_log,start_time,final_time,Eplus_timestep,Eplus_FileName,solution_idx)
+  res = run_prediction(tim,x,[1],X_sp_log,start_time,final_time,Eplus_timestep,Eplus_FileName,solution_idx)
 
   # utility rate
   uRate = [0.5,0.5,0.6,0.7,1,1]
@@ -59,31 +59,43 @@ def fitness_func(x,solution_idx):
   total_Cost = sum([x*uRate[i] for i,x in enumerate(res)])
   return total_Cost
 
-def penalty_func(ZMAT,output_DF):
+def penalty_func(ZMAT,output_DF,tim):
 
   ## This function could be modified in the future if necessary
-  SP_list = [26.7]*5+[25.6]+[25]+[24]*15+[26.7]*2 # [18,24]
-  ThermalComfort_range = 0.5
-
+  SP_list = [15.6]*5+[17.6]+[19.6]*16+[15.6]*2 # [18,24]
+  ThermalComfort_range = 1
   residuals = 0
   for i in range(output_DF.shape[0]):
     dtime = output_DF.iloc[i,0]
     hourOfDay = int(dtime.hour)
-    residuals += max(ZMAT[i]-SP_list[hourOfDay]-ThermalComfort_range,0)
+    if SP_list[hourOfDay] >20:
+      a=0
+    residuals += max(SP_list[hourOfDay]-ThermalComfort_range-ZMAT[i],0)
   
   return residuals
 
 def fitness_wrapper(x,solution_idx,hyperParam):
   # run simulation 
-  cooling_rate, ZMAT,output_DF = run_prediction(x,solution_idx,hyperParam)
-
+  Sim_Status,Heating_rate, ZMAT,output_DF = run_prediction(x,solution_idx,hyperParam)
   # utility rate, read from an external file
   uRate = [3.462]*6+[5.842]*9+[10.378]*5+[5.842]*2+[3.462]*2  # Summer, workday. Replaced later. 
   
-  alpha = 10**20 ## This value should be adjusted.. 
+  alpha = 10**20 ## 
+  if Sim_Status:
+    tim = hyperParam["tim"]
+    PH = hyperParam["PH"]
+    # Total electricity rate = E_{RadSys_Pump} + E_{Boiler} +E_{Plant pump}
+    PowerConsumption = output_DF.iloc[:,4:].apply(sum, axis = 1)
+    total_Cost = 0
+    CurMon,CurDay,HourOfDay = convert_NumOfSec_To_MonAndDay(tim)
+    for i in range(PH):
+      curHour = (HourOfDay + i)%24
+      #print(curHour,"PowerCom:",PowerConsumption)
+      total_Cost = total_Cost + (uRate[curHour])*PowerConsumption.iloc[i]
 
-  total_Cost = - sum([x*uRate[i] for i,x in enumerate(cooling_rate)]) - alpha * penalty_func(ZMAT,output_DF)
-
+    total_Cost = - total_Cost - alpha * penalty_func(ZMAT,output_DF,tim)
+  else:
+    total_Cost = -alpha
   return total_Cost
 
 def run_prediction(CVar_list, solution_idx,hyperParam):
@@ -114,51 +126,69 @@ def run_prediction(CVar_list, solution_idx,hyperParam):
 
   ##Step 1. make a copy of the base Eplus model with specific start time and end time
   Cur_WorkPath =  os.getcwd()
-  Target_WorkPath = Cur_WorkPath + '//Model_T{}_{}'.format(tim/3600,solution_idx)
+  Target_WorkPath = Cur_WorkPath + '\\Model_T{}_{}'.format(tim/3600,solution_idx)
 
   if (os.path.exists(Target_WorkPath)):# delete the workpath
     shutil.rmtree(Target_WorkPath)
 
   os.makedirs(Target_WorkPath)
 
-  startMon,startDay = convert_NumOfSec_To_MonAndDay(start_time)
-  endMon,endDay = convert_NumOfSec_To_MonAndDay(final_time)
-  modifyIDF(Cur_WorkPath + "//" + Eplus_FileName,Target_WorkPath+"//"+Eplus_FileName,startMon,startDay,endMon,endDay,Target_WorkPath+"//RadInletWater_SP_schedule.csv")
+  startMon,startDay,HourofDay = convert_NumOfSec_To_MonAndDay(start_time-86400)  # Add one day before the start time as warmup day.
+  endMon,endDay,HourofDay = convert_NumOfSec_To_MonAndDay(final_time)
+  modifyIDF(Cur_WorkPath + "\\" + Eplus_FileName,Target_WorkPath+"\\"+Eplus_FileName,startMon,startDay,endMon,endDay,Target_WorkPath+"\\RadInletWater_SP_schedule.csv")
 
   # write control signal to the .csv file, both historical and new
-  shutil.copyfile(Cur_WorkPath + "//RadInletWater_SP_schedule.csv",Target_WorkPath+"//RadInletWater_SP_schedule.csv")
+  shutil.copyfile(Cur_WorkPath + "\\RadInletWater_SP_schedule.csv",Target_WorkPath+"\\RadInletWater_SP_schedule.csv")
 
-  Input_DF = pd.read_csv(Target_WorkPath+"//RadInletWater_SP_schedule.csv")
+  Input_DF = pd.read_csv(Target_WorkPath+"\\RadInletWater_SP_schedule.csv")
   start_idx,end_idx = int(start_time/3600),int(time_end/3600)
-  Input_DF.iloc[start_idx:end_idx,0] = X_sp  #
-  Input_DF.iloc[start_idx:end_idx,1] = X_sp
-  Aval_Status = [int(xi<=12) for xi in X_sp]
-  Input_DF.iloc[start_idx:end_idx,2] = Aval_Status
+  for i,idx in enumerate(range(start_idx,end_idx)):
+    if X_sp[i]>0:
+      Input_DF.iloc[idx,0] = X_sp[i]
+      Input_DF.iloc[idx,1] = X_sp[i]
+      Input_DF.iloc[idx,2] = int(X_sp[i]>30)
 
-  Input_DF.to_csv(Target_WorkPath+"//RadInletWater_SP_schedule.csv",index = False)
+  Input_DF.to_csv(Target_WorkPath+"\\RadInletWater_SP_schedule.csv",index = False)
 
   ## Step 2. Run EnergyPlus model
    #srun --time 30 --mem-per-cpu 2048 -n 1 energyplus -w USA_CO_Golden-NREL.724666_TMY3.epw 1ZoneUncontrolled.idf
-  argument = ["energyplus", "-w",Cur_WorkPath + "//in.epw","-d",Target_WorkPath,Target_WorkPath+"//"+Eplus_FileName]
+  argument = ["energyplus", "-w",Cur_WorkPath + "\\in.epw","-d",Target_WorkPath,Target_WorkPath+"\\"+Eplus_FileName]
   sp.call(argument)
   
   ## Step 3. After completion, retrieve results
-  # .
-  output_DF = read_result(Target_WorkPath+"//" + "eplusout.eso")
-  tim_idx,end_idx = int((tim-start_time)/3600),int((time_end-start_time)/3600)
-  cooling_Rate = list(output_DF.iloc[tim_idx+48:end_idx+48,2])  # because of two design days start from 48.
-  ZMAT = list(output_DF.iloc[tim_idx+48:end_idx+48,3])  # because of two design days start from 48.
+  Sim_Status = check_SimulationStatus(Target_WorkPath+"\\" + "eplusout.err")
+  print(Sim_Status)
+  if Sim_Status:
+    output_DF = read_result(Target_WorkPath+"\\" + "eplusout.eso")
+    tim_idx,end_idx = int((tim-start_time)/3600),int((time_end-start_time)/3600)
+    ZMAT = list(output_DF.iloc[tim_idx+24:end_idx+24,2]) 
+    Heating_Rate = list(output_DF.iloc[tim_idx+24:end_idx+24,3])   # One warmup day
 
+    ## Step 4. Remove temporary files
+    shutil.rmtree(Target_WorkPath)
+    return Sim_Status, Heating_Rate,ZMAT,output_DF.iloc[tim_idx+24:end_idx+24,:]
+  else:
 
-  ## Step 4. Remove temporary files
-  shutil.rmtree(Target_WorkPath)
-  return cooling_Rate,ZMAT,output_DF.iloc[tim_idx+48:end_idx+48,:]
+    output_DF, ZMAT, Heating_Rate = -1,-1,-1
+    ## Step 4. Remove temporary files
+    shutil.rmtree(Target_WorkPath)
+    return Sim_Status, Heating_Rate,ZMAT,output_DF
+
+def check_SimulationStatus(fileName):
+  with open(fileName,'r') as fp:
+    lines = fp.readlines()
+    LastLine = lines[-1]
+  if LastLine.find('Successfully')>-1:
+    Sim_Status = True
+  else:
+    Sim_Status = False
+  return Sim_Status
 
 def read_result(filename):
   import datetime
   ## a function used to process ESO file
 
-  output_idx = [1716,651] # This is ID for Zone Radiant HVAC Cooling Rate,Zone Mean Air
+  output_idx = [675,1748,1753,1991,2033] # Indices for  ZMAT, heating rate, RadSyste Pump E_Rate(w), Boiler E_Rate, Plant Pump E_rate(w)
   data = {'dtime':[],
           'dayType':[]}
   for id_i in output_idx:
@@ -199,12 +229,12 @@ def read_result(filename):
 if __name__ == "__main__":
     
     # simulation setup
-    start_time= 60*60*24*201 
-    final_time= 60*60*24*208
+    start_time= 60*60*24*31 
+    final_time= 60*60*24*32
     Eplus_timestep = 60
 
     # setup for MPC
-    pred_horizon = {"length":24,"timestep":3600}
+    pred_horizon = {"length":1,"timestep":3600}
 
     #### run optimization
     X_sp_log = []  # This trend variable is used to store all setpoints from start_time 
@@ -218,12 +248,13 @@ if __name__ == "__main__":
        12.89107403, 12.47258374, 12.92129057, 11.79345203]
 
     tim = start_time
-    Eplus_FileName = "testModel_v94_2day_V940_CFD_NoDOAS.idf"
+    Eplus_FileName = "MediumOff_NewYork.idf"
 
 
     #prepare hyper parameter
     hyperParam  ={}
     hyperParam["tim"] = tim
+    hyperParam["PH"] = pred_horizon['length']
     hyperParam["CVar_timestep"] =  CVar_timestep 
     hyperParam["X_sp_log"] = X_sp_log 
     hyperParam["start_time"] = start_time 
