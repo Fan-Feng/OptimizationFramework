e3557e0c61d6f2b756149844d3b5063d2eae80bc
fan feng <ffeng@tamu.edu>
2021-09-09 01:13:13 -0500

    update

--- Experiment_Heating/PH1_Oneday/PH1_C90_M20_P100_G50_Feb04/runTestEplus_parallel.py
+++ Experiment_Heating/PH1_Oneday/PH1_C90_M20_P100_G50_Feb04/runTestEplus_parallel.py
@@ -321,8 +321,8 @@ with MPIPool() as pool:
     print("Op completed")
     SP_cur = ga_instance.best_solution()[0][0]
 
-    #fitness_value = fitness_wrapper([SP_cur],0,hyperParam)
-    fitness_value = 0
+    fitness_value = fitness_wrapper([SP_cur],0,hyperParam)
+    #fitness_value = 0
     X_sp_log.append(SP_cur)
     print(X_sp_log,tim,fitness_value)
     # proceed to next timestep
