1df0703ec40dbffc2a509f3039ef7e5db3160315
fan feng <ffeng@tamu.edu>
2021-12-10 05:49:21 -0600

    up

--- Multizone_case/CZ1_Pri_NYcity/Heating/runTestEplus_parallel.py
+++ Multizone_case/CZ1_Pri_NYcity/Heating/runTestEplus_parallel.py
@@ -192,7 +192,7 @@ def read_result(filename):
   import datetime
   ## a function used to process ESO file
 
-  output_idx =   output_idx = [1140,1141,1142,1143,1144,2426,2468,2474,2480,2486,2732,2759] # Indices for  
+  output_idx =   output_idx = [1140,1141,1142,1143,1144,2426,2468,2474,2480,2486,2732,2760] # Indices for  
   data = {'dtime':[],
           'dayType':[]}
   for id_i in output_idx:
